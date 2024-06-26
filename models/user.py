28905eecf743a705a5f6549a872e1b4657b9c2aa
semkufu95 <semkufu95@gmail.com>
2024-06-26 14:34:52 -0700

    Correction of user.py

--- models/user.py
+++ models/user.py
@@ -5,22 +5,22 @@ from sqlalchemy import Column, String
 from models import models
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel):
     """This class defines a user by various attributes"""
     def __init__(self, *args, **kwargs):
         '''Initialize  user'''
         super().__init__(*args, **kwargs)
 
-        if models.storage_t == 'data_base':
-            __tablename__ = 'users'
-            email = Column(String(128), nullable=False)
-            password = Column(String(128), nullable=False)
-            first_name = Column(String(128), nullable=True)
-            last_name = Column(String(128), nullable=True)
-            places = relationship('Place', cascade='all, delete, delete-orphan', backref='user')
-            reviews = relationship('Review', cascade='all, delete, delete-orphan', backref='user')
-        else:
-            email = ''
-            password = ''
-            first_name = ''
-            last_name = ''
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
